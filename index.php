648e8ddeb314d0f0f5805dd68d2d993ecc11c7c7
Santoni92 <98034624+Santoni92@users.noreply.github.com>
2022-05-13 16:41:27 +0200

    aggiunto costruttore ed metodo

--- index.php
+++ index.php
@@ -1,8 +1,29 @@
 <?php
+//definisco la classe
     class Movie{
         //attributi(variabili d'istanza)
-        public $titolo;
+        public $titolo ;
         public $regista;
+        //public $paeseDiProduzione;
+        public $visualizzazioni = 0;
 
+        //costruttore
+        function __construct($titolo,$regista)
+        {
+            $this -> $titolo = $titolo;
+            $this -> $regista = $regista;             
+        }
+
+        //metodi
+        public function incrementViews()
+        {
+            $this -> $visualizzazioni++;
+        }
     }
+
+    //istanzio un oggetto della classe
+    $film = new Movie('via col vento','Victor Flaming');
+
+
+    echo('Il film ' . $film -> titolo .  'è stato visto ' . $film -> visualizzazioni .  ' volte');   
 ?>
